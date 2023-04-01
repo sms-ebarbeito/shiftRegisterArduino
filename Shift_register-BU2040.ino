9f190832278938e93a8c486cf20d85c78e93e7fd
Enrique Barbeito <ebarbeito@sms-sudamerica.com>
2023-04-01 20:02:02 -0300

    improve using defines outputs

--- Shift_register-BU2040.ino
+++ Shift_register-BU2040.ino
@@ -7,26 +7,24 @@ pulse has 20uS
 USING INTERRUPTS
 **/
 
-//#include <digitalWriteFast.h>
-
 //DEFINE PORTS
 #define DATA 2 //PIN 2
 #define CLK  3 //PIN 3
 
-#define O11 15 //PIN A1
-#define O10 14 //PIN A0
-#define O09 13 //PIN 13
-#define O08 12 //PIN 12
-#define O07 11 //PIN 11
-#define O06 10 //PIN 10
-#define O05 9 //PIN 9
-#define O04 8 //PIN 8
-#define O03 7 //PIN 7
-#define O02 6 //PIN 6
-#define O01 5 //PIN 5
-#define O00 4 //PIN 4
+#define O11 1<<4 //PIN A1
+#define O10 1<<5 //PIN A0
+#define O09 1<<6 //PIN 13
+#define O08 1<<7 //PIN 12
+#define O07 1<<0 //PIN 11
+#define O06 1<<1 //PIN 10
+#define O05 1<<2 //PIN 9
+#define O04 1<<3 //PIN 8
+#define O03 1<<4 //PIN 7
+#define O02 1<<5 //PIN 6
+#define O01 1<<0 //PIN 5
+#define O00 1<<1 //PIN 4
 
-int outputs[] = { LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW };
+byte outputs[] = { LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW };
 int output = 0;
 byte data = 0x0;
 
@@ -78,18 +76,18 @@ void ISR_handleClk() {
 void writeOut() {
   //UGLY, BUT FASTER!
   //PORTD
-  if (outputs[0] == HIGH) { PORTD |= 1<<4; } else { PORTD &= ~1<<4;}
-  if (outputs[1] == HIGH) { PORTD |= 1<<5; } else { PORTD &= ~1<<5;}
-  if (outputs[2] == HIGH) { PORTD |= 1<<6; } else { PORTD &= ~1<<6;}
-  if (outputs[3] == HIGH) { PORTD |= 1<<7; } else { PORTD &= ~1<<7;}
+  if (outputs[0] == HIGH) { PORTD |= O11; } else { PORTD &= ~O11;}
+  if (outputs[1] == HIGH) { PORTD |= O10; } else { PORTD &= ~O10;}
+  if (outputs[2] == HIGH) { PORTD |= O09; } else { PORTD &= ~O09;}
+  if (outputs[3] == HIGH) { PORTD |= O08; } else { PORTD &= ~O08;}
   //PORTB
-  if (outputs[4] == HIGH) { PORTB |= 1<<0; } else { PORTB &= ~1<<0;}
-  if (outputs[5] == HIGH) { PORTB |= 1<<1; } else { PORTB &= ~1<<1;}
-  if (outputs[6] == HIGH) { PORTB |= 1<<2; } else { PORTB &= ~1<<2;}
-  if (outputs[7] == HIGH) { PORTB |= 1<<3; } else { PORTB &= ~1<<3;}
-  if (outputs[8] == HIGH) { PORTB |= 1<<4; } else { PORTB &= ~1<<4;}
-  if (outputs[9] == HIGH) { PORTB |= 1<<5; } else { PORTB &= ~1<<5;}
+  if (outputs[4] == HIGH) { PORTB |= O07; } else { PORTB &= ~O07;}
+  if (outputs[5] == HIGH) { PORTB |= O06; } else { PORTB &= ~O06;}
+  if (outputs[6] == HIGH) { PORTB |= O05; } else { PORTB &= ~O05;}
+  if (outputs[7] == HIGH) { PORTB |= O04; } else { PORTB &= ~O04;}
+  if (outputs[8] == HIGH) { PORTB |= O03; } else { PORTB &= ~O03;}
+  if (outputs[9] == HIGH) { PORTB |= O02; } else { PORTB &= ~O02;}
   //PORTC --> PORTC is analog, maybe is slow, maybe is beter to use PORTD0, PORTD1
-  if (outputs[11] == HIGH) { PORTC |= 1<<0; } else { PORTC &= ~1<<0;}
-  if (outputs[12] == HIGH) { PORTC |= 1<<1; } else { PORTC &= ~1<<1;}
+  if (outputs[11] == HIGH) { PORTC |= O01; } else { PORTC &= ~O01;}
+  if (outputs[12] == HIGH) { PORTC |= O00; } else { PORTC &= ~O00;}
 }
